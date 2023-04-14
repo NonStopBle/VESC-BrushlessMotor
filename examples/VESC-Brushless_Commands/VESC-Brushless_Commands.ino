2c95084b6fff58fbc69caa2d1cabc636a37afbe7
Retr0 <rattanapon8205@gmail.com>
2023-04-14 14:48:28 +0700

    Update VESC-Brushless_Commands.ino

--- examples/VESC-Brushless_Commands/VESC-Brushless_Commands.ino
+++ examples/VESC-Brushless_Commands/VESC-Brushless_Commands.ino
@@ -1,31 +1,38 @@
 #include <VESC-BrushlessMotor.h>
 
-// Define a VESCBrushlessMotor object with the VESC ID
-// You can find the VESC ID in the VESC Tool
+// Define Canbus ports number
+#define CAN_NUMBER CAN1
+
+// Define the VESC motor object with the VESC ID
 VESCBrushlessMotor Motor_A(110);
 
 void setup() {
-  // Initialize the CAN bus
-  Motor_A.initCanBus();
+  // Initialize the canbus for the motor
+  // By default baudrate has been set at 1000000 bps and 16 MailBox
+  Motor_A.initCanBus(1000000 , 16);
 }
 
 void loop() {
-  // Poll the CAN bus for incoming data
+  // Get motor data from the VESC
   Motor_A.PollCanBus();
 
-  // Set the motor speed to 100 RPM
-  Motor_A.setMotorSpeed(100);
+  // Print motor data to the serial monitor
+  Serial.print("Speed: ");
+  Serial.print(Motor_A.getMotorSpeed());
+  Serial.print(" RPM, Current: ");
+  Serial.print(Motor_A.getMotorCurrent());
+  Serial.print(" A, Duty Cycle: ");
+  Serial.print(Motor_A.getMotorDutyCycle());
+  Serial.println(" %");
 
-  // Set the motor current to 2 A
-  //Motor_A.setMotorCurrent(2);
-
-  // Set the motor duty cycle to 10%
-  //Motor_A.setMotorDuty(10);
-
-  // Print the motor speed, current, and duty cycle to the Serial Monitor
-  Serial.print("Speed : " + String(Motor_A.getMotorSpeed()) + String(" RPM"));
-  Serial.print(" , ");
-  Serial.print("Current : " + String(Motor_A.getMotorCurrent()) + String(" A"));
-  Serial.print(" , ");
-  Serial.println("Duty Cycle : " + String(Motor_A.getMotorDutyCycle()) + String(" %"));
+  // Set motor parameters as needed
+  
+  // Set different motor parameters
+  //Motor_A.setMotorSpeed(100);  // set motor a speed unit rpm (RPM) this library already convert from ERPM to RPM
+  //Motor_A.setMotorCurrent(2); //set motor current unit ampere (A)
+  //Motor_A.setMotorCurrentREL(2); //set motor current unit ampere (A)
+  //Motor_A.setMotorCurrentBrakeREL(10);  // set motor current handbrake rel unit percentage (%)
+  //Motor_A.setMotorCurrentBrake(5); // set motor current brake unit amepere (A)
+  //Motor_A.setMotorCurrentHandBrake(10); // set motor current handbrake unit amepere (A)
+  //Motor_A.setMotorDuty(10); // set motor by duty cycle unit percentage (%)
 }
